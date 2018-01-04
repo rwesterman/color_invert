66a9902bc77579caa82a869617f76bec1d26b9d4
rwesterman <rwesterman7@gmail.com>
2018-01-04 12:42:07 -0600

    Made ColorInvert more user friendly.

--- ColorInvert.py
+++ ColorInvert.py
@@ -1,8 +1,8 @@
-#! Python3
+#! /usr/bin/env python3
 
+import os, sys, os.path
 from PIL import Image
 import PIL.ImageOps
-import os, sys, os.path
 
 def invert_colors(path):
     """Given a folder, inverts all .jpg and .png images in base and subfolders"""
@@ -32,23 +32,45 @@ def invert_colors(path):
 
     return("Inverted {} RGBA images, and {} standard images".format(count_rgba, count_std))
 
+def cmd_launch():
 
-
-path = ''                       # Avoid IOError if no path given, this will be caught with if statement below
-
-try:
-    path = sys.argv[1]          #Takes first argument after script name as path
-except IndexError as e:
-    print("Index error. No target path found")  #Catches index error if it occurs
-
-if path.lower() == 'help' or not path:
-    print("Please provide a path to your images as an argument after the script path")
-    print("Eg. python c:/ColorInvert.py c:/images")
-
-
-
-else:
     try:
-        print(invert_colors(path))
-    except OSError as e:
-        print("Error: given path does not exist")
+        path = input("Please enter the full filepath to a directory containing the images to be inverted:")
+    except IndexError as e:
+        print("Index error. No target path found")  #Catches index error if it occurs
+    return path
+
+
+def launch_choices():
+
+    # determine whether script is run from image directory or not
+    launch_choice = str(
+        input("Are you running color_invert.py from the directory with the images you want to invert? (Y/n)")).lower()
+
+    # Invert images in working directory
+    if launch_choice == 'y':
+        print(invert_colors(os.getcwd()))
+
+    # Send to cmd_launch to prompt user for filepath
+    elif launch_choice == 'n':
+        print(invert_colors(cmd_launch()))
+
+    # Give user the option to exit without inverting
+    elif launch_choice == 'exit':
+        print("Exiting program...")
+
+    # if answer is not y, n, or exit, try again
+    else:
+        print("Please only answer Y or n or type exit to quit")
+        launch_choices()
+
+def main():
+    path = ''                       # Avoid IOError if no path given, this will be caught with if statement below
+    print("Welcome to Color Invert, you can find the latest version and documentation at https://github.com/rwesterman/color_invert")
+    launch_choices()
+
+
+if __name__ == '__main__':
+    main()
+
+
